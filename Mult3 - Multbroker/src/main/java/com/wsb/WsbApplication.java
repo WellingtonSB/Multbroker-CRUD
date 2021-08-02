f36d62c6f012837793149cfd769495c17fbc8b41
Wellington <wellingtonsouza2504@gmail.com>
2021-08-02 17:40:02 -0300

    initial commit

---
+++ Mult3 - Multbroker/src/main/java/com/wsb/WsbApplication.java
@@ -0,0 +1,13 @@
+package com.wsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsbApplication.class, args);
+	}
+
+}
